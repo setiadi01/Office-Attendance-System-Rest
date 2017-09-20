db746f1c5657142e65bee3974b404270d8a4aef7
fandyindra <fandyindra0794@gmail.com>
2017-09-20 17:47:34 +0700

    penambahan loader

--- public/app/js/login.controller.js
+++ public/app/js/login.controller.js
@@ -6,6 +6,7 @@
 
 	function LoginCtrl($scope, $auth, $state, $interval){
 		var ui = $scope;
+		$scope.loader = false;
 
 		if (localStorage.getItem('user') != null){
 			$state.go('home');
@@ -17,20 +18,23 @@
 		}, 1000);
 
 		ui.login = function(){
+			$scope.loader = true;
 			$auth.login(ui.input)
 			.then(function(response) {
 				console.log(response);
 				if (response.data.status == 'OK') {
 					// localStorage.setItem('user', response.data.user.username);
 					localStorage.setItem('user', JSON.stringify(response.data.user));
-
+					$scope.loader = false;
 					$state.go('home');
 				}else{
+					$scope.loader = false;
 					alert('kombinasi password dan username salah')
 				}
 			})
 			.catch(function(response) {
 				console.log(response);
+				$scope.loader = false;
 				alert('Maaf, server sedang mengalami gangguan')
 			});
 		}
